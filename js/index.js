8c91dae684c00d89294d82e821799ca80defe71c
Godwin Martins <gmocdes20@gmail.com>
2021-01-30 02:55:05 +0100

    final touch

--- js/index.js
+++ js/index.js
@@ -2,3 +2,18 @@ window.addEventListener('scroll', function () {
   const header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 0);
 })
+
+
+const nav = document.querySelector('.navigation');
+const mToggle = document.querySelector('.menuToggle');
+mToggle.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+
+  mToggle.classList.toggle('active')
+  nav.classList.toggle('active')
+
+  console.log(mToggle.classList)
+  console.log(nav.classList)
+
+}
